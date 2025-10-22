a8d88bf1fe669a7daea62170b254786cbc958869
Diana111U <diana_ustinova11@mail.ru>
2025-10-22 18:41:25 +0300

    Создание классов

---
+++ DataGridView/DataGridView/Forms/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using DataGridView.Models;
+
+namespace DataGridView
+{
+    public partial class MainForm : Form
+    {
+        private readonly List<StudentModel> items;
+        public MainForm()
+        {
+            items = new List<StudentModel>();
+            items.Add(new StudentModel
+            { 
+                FullName ="Иванов Иван Иванович",
+                Gender = Gender.Male,
+                BirthDay = DateOnly.Parse("10.10.2005"),
+                FormOfEducation = FormOfEducation.FullTime,
+                MathExamScore = 62,
+                RussianLanguageExamScore = 87,
+                InformaticExamScore = 99
+            });
+            items.Add(new StudentModel
+            {
+                FullName = "Петрова Анна Михайловна",
+                Gender = Gender.Female,
+                BirthDay = DateOnly.Parse("10.10.2004"),
+                FormOfEducation = FormOfEducation.PartTime,
+                MathExamScore = 90,
+                RussianLanguageExamScore = 93,
+                InformaticExamScore = 100
+            });
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ DataGridView/DataGridView/Models/ApplicantModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridView.Models
+{
+    internal class StudentModel
+    {
+        public string FullName { get; set; } = string.Empty;
+
+        public Gender Gender { get; set; }
+
+        public DateOnly BirthDay { get; set; }
+
+        public FormOfEducation FormOfEducation { get; set; }
+
+        public int MathExamScore { get; set; }
+
+        public int RussianLanguageExamScore { get; set; }
+
+        public int InformaticExamScore { get; set; }
+
+        public int TotalAmount { get; set; }
+    }
+}
