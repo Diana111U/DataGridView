cecca3bb8e4e94993850119e05cd6eb87d9f59b1
Diana111U <diana_ustinova11@mail.ru>
2025-12-10 00:20:42 +0300

    Добавлены файлы DataGridViewContext и DataGridViewStorage

--- DataGridView/DataGridView.DataBaseStorage/DataGridViewContext.cs
+++ DataGridView/DataGridView.DataBaseStorage/DataGridViewContext.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataGridView.AppConstants;
+using DataGridView.Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataGridView.DataBaseStorage
 {
-    internal class DataGridViewContext
+    public class DataGridViewContext : DbContext
     {
+        /// <summary>
+        /// Сущность <see cref="ApplicantModel"/>.
+        /// </summary>
+        public DbSet<ApplicantModel> Applicants { get; set; }
+
+        /// <summary>
+        /// Создаёт экземпляр <see cref="DataGridViewContext"/>.
+        /// </summary>
+        public DataGridViewContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=DataGridViewProjectDatabase;Trusted_Connection=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicantModel>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.FullName)
+                    .IsRequired()
+                    .HasMaxLength(Constants.FullNameMaxLength);
+
+                entity.Property(e => e.Gender)
+                    .HasConversion<int>();
+
+                entity.Property(e => e.FormOfEducation)
+                    .HasConversion<int>();
+    
+                entity.Property(e => e.BirthDay)
+                    .IsRequired();
+            });
+        }
     }
 }
