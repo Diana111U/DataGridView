e155985f57de9128c1403b66fbbef0298ddc205b
Diana111U <diana_ustinova11@mail.ru>
2025-12-15 18:46:12 +0300

    Добавлено добавление абитуриента

--- DataGridView/DataGridView.Web/Controllers/HomeController.cs
+++ DataGridView/DataGridView.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using DataGridView.Manager.Contracts;
+﻿using DataGridView.Entities.Models;
+using DataGridView.Manager;
+using DataGridView.Manager.Contracts;
 using DataGridView.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,11 +11,17 @@ namespace DataGridView.Web.Controllers
     {
         private readonly IApplicantManager applicantManager;
 
+        /// <summary>
+        /// Инициализирует контроллер
+        /// </summary>
         public HomeController(IApplicantManager applicantManager)
         {
             this.applicantManager = applicantManager;
         }
 
+        /// <summary>
+        /// Отображает главную страницу со списком абитуриентов и статистикой
+        /// </summary>
         public async Task<IActionResult> Index()
         {
             var applicants = await applicantManager.GetAllApplicants();
@@ -26,14 +34,41 @@ namespace DataGridView.Web.Controllers
             };
 
             return View(model);
-
         }
 
+        /// <summary>
+        /// Отображает форму для добавления нового товара
+        /// </summary>
+        [HttpGet]
+        public IActionResult Create() => View(new ApplicantModel());
+
+        /// <summary>
+        /// Принимает данные нового абитуриента из формы и добавляет его в хранилище
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ApplicantModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await applicantManager.AddApplicant(model);
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// Отображает страницу с политикой конфиденциальности
+        /// </summary>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Отображает страницу ошибки с информацией о текущем запросе
+        /// </summary>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
