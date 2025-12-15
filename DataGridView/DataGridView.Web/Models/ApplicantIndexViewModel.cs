fd829c878a4dd71f3c606c73df7b09388396f26e
Diana111U <diana_ustinova11@mail.ru>
2025-12-15 18:20:41 +0300

    Добавила класс ApplicantIndexViewModel

---
+++ DataGridView/DataGridView.Web/Models/ApplicantIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DataGridView.Entities.Models;
+using DataGridView.Manager.Contracts;
+
+namespace DataGridView.Web.Models
+{
+    /// <summary>
+    /// Модель представления для главной страницы абитуриентов
+    /// </summary>
+    public class ApplicantIndexViewModel
+    {
+        /// <summary>
+        /// Список всех абитуриентов, отображаемых в таблице
+        /// </summary>
+        public List<ApplicantModel> Applicants { get; set; } = [];
+
+        /// <summary>
+        /// Общая статистика по абитуриентам
+        /// </summary>
+        public ApplicantStatistics Statistics { get; set; } = new();
+    }
+}
